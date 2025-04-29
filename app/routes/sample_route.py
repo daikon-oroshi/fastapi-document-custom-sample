2e857ab5c2de0123d8d37146368431a7c599743a
oroshi <oroshinekko@gmail.com>
2025-04-29 19:47:17 +0900

    api description markdon

--- app/routes/sample_route.py
+++ app/routes/sample_route.py
@@ -1,9 +1,7 @@
 from fastapi import APIRouter, Query
 from typing import Annotated
 from fastapi.responses import PlainTextResponse
-
 from schema import SampleQuery, SampleRequest, SampleResponse
-
 from tags import SAMPLE_TAG, SAMPLE_TAG2
 
 
@@ -11,10 +9,22 @@ router1 = APIRouter(prefix="/sample", tags=[SAMPLE_TAG.name])
 router2 = APIRouter(prefix="/sample2", tags=[SAMPLE_TAG2.name])
 
 
+GET_SAMPLE_DESCRIPTION = """
+## Sample description here
+- aaa
+- bbb
+- ccc
+
+### subtitle 3
+* aaa description here
+* aaa description here
+"""
+
+
 @router1.get(
     "/",
-    summary="get_sample sammary here",
-    description="get_sample description here",
+    summary="get_sample sammary here \n",
+    description=GET_SAMPLE_DESCRIPTION,
     operation_id="get_sample",
     response_class=PlainTextResponse,
 )
